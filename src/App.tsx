6ea097e9ed6c29408789dff417a6c3bce57443b8
Yussin R. Hidayat <van.de.bugs@gmail.com>
2020-08-29 21:13:35 +0700

    Move App to src directory

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>This is App screen.</Text>
+    </View>
+  );
+};
+
+export default App;
